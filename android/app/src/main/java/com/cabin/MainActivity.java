f8778f57720160324fc94ac0b2b5c5ed5ef0566b
huhuaishun <huhuaishun@icloud.com>
2016-11-09 16:29:08 +0800

    update project

--- android/app/src/main/java/com/cabin/MainActivity.java
+++ android/app/src/main/java/com/cabin/MainActivity.java
@@ -1,28 +1,19 @@
 package com.cabin;
 
 import com.facebook.react.ReactActivity;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.github.yamill.orientation.OrientationPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
-import com.rnfs.RNFSPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.reactnative.imagepicker.PickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.burlap.filetransfer.FileTransferPackage;
+import com.react.rnspinkit.RNSpinkitPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.github.yamill.orientation.OrientationPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
+import com.rnfs.RNFSPackage;
 import com.microsoft.codepush.react.CodePush;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
+import com.cmcewen.blurview.BlurViewPackage;
 
 public class MainActivity extends ReactActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -30,35 +21,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "cabin";
-    }
-
-    /**
-     * Returns whether dev mode should be enabled.
-     * This enables e.g. the dev menu.
-     */
-    @Override
-    protected boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    /**
-     * A list of packages used by the app. If the app uses additional views
-     * or modules besides the default ones, add more packages here.
-     */
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new BlurViewPackage(),
-            new OrientationPackage(this),
-            new RNSpinkitPackage(),
-            new RNFSPackage(),
-            new LinearGradientPackage(),
-            new VectorIconsPackage(),
-            new FileTransferPackage(),
-            new ReactNativePushNotificationPackage(this),
-            new CodePush(null, this, BuildConfig.DEBUG)
-        );
+        return "Cabin";
     }
 }
--- android/app/src/main/java/com/cabin/MainApplication.java
+++ android/app/src/main/java/com/cabin/MainApplication.java
@@ -4,13 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.burlap.filetransfer.FileTransferPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.microsoft.codepush.react.CodePush;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
-import com.cboy.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -37,13 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new RNSpinkitPackage(),
-           new MainReactPackage(),
-            new FileTransferPackage(),
-            new PickerPackage(),
-            new MapsPackage(),
-            new CodePush("_xhxqrft6XsFcbkeYOoH-U5Us-_wN1cJKQhAb", getApplicationContext(), BuildConfig.DEBUG),
-            new PickerPackage(),
-            new SplashScreenReactPackage()
+           new MainReactPackage()
       );
     }
   };
