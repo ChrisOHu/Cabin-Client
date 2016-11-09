eee205baead457e004951f8c006d5c93b7f94556
huhuaishun <huhuaishun@icloud.com>
2016-11-09 18:28:05 +0800

    rename ios/CabinTests/riseTests.m -> ios/CabinTests/CabinTests.m

--- ios/CabinTests/CabinTests.m
+++ ios/CabinTests/CabinTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface riseTests : XCTestCase
+@interface CabinTests : XCTestCase
 
 @end
 
-@implementation riseTests
+@implementation CabinTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
